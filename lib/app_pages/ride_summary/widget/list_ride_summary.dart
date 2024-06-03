17f088c765e4351241d2f059de06f0ac697ee055
Aashima <aashimaamih@gmail.com>
2024-06-03 15:30:17 +0530

    end ride and pdf open file done

---
+++ lib/app_pages/ride_summary/widget/list_ride_summary.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListViewWidget extends StatelessWidget {
+  final String label;
+  final String value;
+  const ListViewWidget({super.key, required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Text(
+            label.toString(),
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Text(
+            maxLines: 10,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            value.toString() == "" ? "-" : value.toString(),
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
