481287687239d71c9b780966586ceb53d0d5b813
eswarapandiyan <113456788+eswarapandiyan@users.noreply.github.com>
2024-09-19 19:27:01 +0530

    [Eswar] => Vehicle page(Scroll issue fixed)

--- lib/app_pages/vehicle_page/select_vehicle_closer_matches.dart
+++ lib/app_pages/vehicle_page/select_vehicle_closer_matches.dart
@@ -75,99 +75,94 @@ class _VehicleCloserMatchesState extends State<VehicleCloserMatches> {
             },
           ),
         ),
-        body: Stack(
+        body: Column(
           children: [
             /// Filtered vehicle
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Column(
-                children: [
-                  CampusWidget(data: data, logo: logoURL),
-                  const SizedBox(height: 16),
-                  if (fd.isNotEmpty) ...[
-                    Expanded(
-                      child: GridView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 8.0,
-                          crossAxisSpacing: 8.0,
-                          mainAxisExtent: 200,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    CampusWidget(data: data, logo: logoURL),
+                    const SizedBox(height: 16),
+                    if (fd.isNotEmpty) ...[
+                      Expanded(
+                        child: GridView.builder(
+                          physics: BouncingScrollPhysics(),
+                          scrollDirection: Axis.vertical,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 8.0,
+                            crossAxisSpacing: 8.0,
+                            mainAxisExtent: 200,
+                          ),
+                          itemCount: fd.length,
+                          itemBuilder: (context, index) {
+                            return DynamicCardWidget(
+                              imageUrl: fd[index]['imageUrl'].toString(),
+                              plan: fd[index]["planType"].toString(),
+                              distanceRange:
+                                  fd[index]['distanceRange'].toString(),
+                              vehicleId: fd[index]['vehicleId'].toString(),
+                              onTap: () {
+                                cardClick(fd[index]['vehicleId'].toString());
+                              },
+                            );
+                          },
                         ),
-                        itemCount: fd.length,
-                        itemBuilder: (context, index) {
-                          return DynamicCardWidget(
-                            imageUrl: fd[index]['imageUrl'].toString(),
-                            plan: fd[index]["planType"].toString(),
-                            distanceRange:
-                                fd[index]['distanceRange'].toString(),
-                            vehicleId: fd[index]['vehicleId'].toString(),
-                            onTap: () {
-                              cardClick(fd[index]['vehicleId'].toString());
-                            },
-                          );
-                        },
                       ),
-                    ),
-                  ] else ...[
-                    const NoMatches()
+                    ] else ...[
+                      const NoMatches()
+                    ],
                   ],
-                ],
+                ),
               ),
             ),
 
             /// Closer Matches vehicle
             if (cd.isNotEmpty) ...[
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: 250,
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        child: Text(
-                          "Closer Matches :",
-                          style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+              Container(
+                height: 250,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      child: Text(
+                        "Closer Matches :",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      // const SizedBox(height: 8),
-                      Expanded(
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: cd.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 5,
-                              ),
-                              child: DynamicCardWidget(
-                                imageUrl: cd[index]['imageUrl'] ?? "",
-                                plan: cd[index]["planType"],
-                                distanceRange: cd[index]['distanceRange'],
-                                vehicleId: cd[index]['vehicleId'],
-                                onTap: () {
-                                  cardClick(cd[index]['vehicleId']);
-                                },
-                              ),
-                            );
-                          },
-                        ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: cd.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: DynamicCardWidget(
+                              imageUrl: cd[index]['imageUrl'] ?? "",
+                              plan: cd[index]["planType"],
+                              distanceRange: cd[index]['distanceRange'],
+                              vehicleId: cd[index]['vehicleId'],
+                              onTap: () {
+                                cardClick(cd[index]['vehicleId']);
+                              },
+                            ),
+                          );
+                        },
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
